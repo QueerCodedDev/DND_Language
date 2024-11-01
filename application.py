e83a3db890c87c5c63b29342d432bac12c838dd2
QueerCodedDev <queercodeddev@gmail.com>
2024-11-01 14:17:17 -0500

    fixed the logical error that was allowing for the stroke count to be incorrect

--- application.py
+++ application.py
@@ -4,38 +4,41 @@ import time
 import pygame
 import csv
 
-font = []
+font = {}
 # Load csv font data
 with open('font.csv', newline='') as csvfile:
     buffer = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in buffer:
-        font.append(row)
+        font[row[0]] = row[1]
 
 # Load special character font data
-font_special = [
-    ['.', 'WA:AD:WS'],
-    [',', 'WA:AD:WD:WS'],
-    [':', 'WA:AD:DS:SW'],
-    [';', 'WA:AS:AD:WS'],
-    ['+', 'AD:DW:WS'],
-    ['-', 'WS:SD:DW:AD'],
-    ['*', 'WS:SA:AD:DW'],
-    ['/', 'WS:SA:AD:SD'],
-    ['(', 'WS:AD'],
-    [')', 'WS:AD:AS'],
-    ['=', 'WD:DS:SA:AW:WS:AD'],
-    ['?', 'WA:AS:SD'],
-    ['!', 'WA:WD:DS']
-]
+font_special = {
+    ' ': '',
+    '.': 'WA:AD:WS',
+    ',': 'WA:AD:WD:WS',
+    ':': 'WA:AD:DS:SW',
+    ';': 'WA:AS:AD:WS',
+    '+': 'AD:DW:WS',
+    '-': 'WS:SD:DW:AD',
+    '*': 'WS:SA:AD:DW',
+    '/': 'WS:SA:AD:SD',
+    '(': 'WS:AD',
+    ')': 'WS:AD:AS',
+    '=': 'WD:DS:SA:AW:WS:AD',
+    '?': 'WA:AS:SD',
+    '!': 'WA:WD:DS',
+    '"': 'WD:WA:WS',
+    '\'': 'WD:DS:SA'
+}
 
 # Add special characters to main list
-font.extend(font_special)
+font.update(font_special)
 
 # Define positions
-W = (0,  0)
+W = (0, 0)
 A = (0, -1)
 S = (1, -1)
-D = (1,  0)
+D = (1, 0)
 
 STROKES = {
     '': [],
@@ -58,10 +61,29 @@ screen_height = cell_size * 16
 # Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+one_stroke = white
+two_stroke = (255, 0, 0)  # Red
+
 
 # Create the screen
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Character Rendering")
+
+# History of lines for rendering styling
+stroke_history = []
+
+
+def code_message(s):
+    s = s.upper()
+    coded_message = []
+    for c in s:
+        if c in font:
+            coded_message.append(font[c])
+        else:
+            # print(f"This character [ {c} ] has no representation.")
+            coded_message.append(font[' '])
+
+    return coded_message
 
 
 def draw_grid():
@@ -94,14 +116,42 @@ def render_character(character_lines, grid_x, grid_y):
         except IndexError:
             print(s)
 
-        # Parse and assign coordinates
-        start_x = grid_x * cell_size + stroke[0][0] * cell_size
-        start_y = grid_y * cell_size - stroke[0][1] * cell_size
-        end_x = grid_x * cell_size + stroke[1][0] * cell_size
-        end_y = grid_y * cell_size - stroke[1][1] * cell_size
+        # As long as the stroke isn't empty
+        if stroke != STROKES['']:
+            # Parse and assign coordinates if the stroke isn't empty
+            start_x = grid_x * cell_size + stroke[0][0] * cell_size
+            start_y = grid_y * cell_size - stroke[0][1] * cell_size
+            end_x = grid_x * cell_size + stroke[1][0] * cell_size
+            end_y = grid_y * cell_size - stroke[1][1] * cell_size
 
-        # Render
-        pygame.draw.line(screen, black, (start_x, start_y), (end_x, end_y), 1)
+            # THIS WILL NEED TO BE SIMPLIFIED ONCE IT IS WORKING AS INTENDED
+            # If the line is already in the log, add it as is
+            if [{'x': start_x, 'y': start_y}, {'x': end_x, 'y': end_y}] in stroke_history:
+                stroke_history.append([
+                    {'x': start_x, 'y': start_y},
+                    {'x': end_x, 'y': end_y}
+                ])
+            # If the line is in the log but with the coordinates flipped, enter it as it is in the log
+            elif [{'x': end_x, 'y': end_y}, {'x': start_x, 'y': start_y}] in stroke_history:
+                stroke_history.append([
+                    {'x': start_x, 'y': start_y},
+                    {'x': end_x, 'y': end_y}
+                ])
+            # If this is the first time the line is being entered into the list, enter it as is
+            else:
+                stroke_history.append([
+                    {'x': start_x, 'y': start_y},
+                    {'x': end_x, 'y': end_y}
+                ])
+
+            # Determine stroke color, default is one_stroke
+            stroke_color = one_stroke
+            stroke_count = (stroke_history.count([{'x': start_x, 'y': start_y}, {'x': end_x, 'y': end_y}]) +
+                            stroke_history.count([{'x': end_x, 'y': end_y}, {'x': start_x, 'y': start_y}]))
+            if stroke_count == 2:
+                stroke_color = two_stroke
+            # Render
+            pygame.draw.line(screen, stroke_color, (start_x, start_y), (end_x, end_y), 1)
 
 
 # Main loop
@@ -111,24 +161,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(white)
-    draw_grid()
+    screen.fill(black)
+    # draw_grid()
 
+    # Positional variables
     i = 1
     j = 1
-    for f in font:
-        if f == 'smile' or f == 'frown':
+    increment = 1
+    horizontal_limit = 1
+
+    message = code_message("aa")
+    stroke_history = []
+    for m in message:
+        if m == 'smile' or m == 'frown':
             continue
         else:
-            render_character(f[1], i, j)
-            i += 2
-            if i >= 15:
-                j += 2
+            if m != ' ':
+                render_character(m, i, j)
+            i += increment
+            if i > horizontal_limit:
+                j += increment
                 i = 1
-    # render_character(font[13][1], 0, 0)
 
-# To check if a line is being redone, save list of drawn lines and before drawing a line check and see if another line
-    # has been drawn there and change the color accordingly.
+    print(stroke_history)
 
     pygame.display.flip()
     # running = False
